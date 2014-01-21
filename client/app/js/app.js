99b17e56897008783f554bf956fa697afc28bd74
Pascal Le Merrer <pascal.lemerrer@gmail.com>
2014-01-21 20:35:33 +0100

    renamed first angular view to "stories"

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -11,7 +11,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'StoryCtrl'});
+  $routeProvider.when('/stories', {templateUrl: 'partials/stories.html', controller: 'StoryCtrl'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]).
